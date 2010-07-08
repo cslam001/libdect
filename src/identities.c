3b587ea913e6f98cf2f75f1b54390ba672d9a864
Patrick McHardy <kaber@trash.net>
2010-07-08 21:30:57 +0200

    Update copyright statements
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- src/identities.c
+++ src/identities.c
@@ -1,7 +1,7 @@
 /*
  * DECT Identities
  *
- * Copyright (c) 2009 Patrick McHardy <kaber@trash.net>
+ * Copyright (c) 2009-2010 Patrick McHardy <kaber@trash.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
